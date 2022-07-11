7c3da2a529ae9c094e06d47576fd5a058ab951e4
paranshu1234 <paranshu1234@gmail.com>
2022-07-11 15:44:13 +0530

    base landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,11 @@ module.exports = {
   siteMetadata: {
     title: `APM Software`,
     description: `We will build your business in the virtual world`,
-    author: `@gatsbyjs`,
+    author: `@apmsoftware`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -28,7 +29,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
